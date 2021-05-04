5307b20ac5fa048fa930233b7547557b9cbd50a6
Chris Johnson <chrisjohnson1138@gmail.com>
2021-05-04 09:16:11 +0100

    Added listeners to number keys and started function to display inputs on the screen

--- calc.js
+++ calc.js
@@ -1,9 +1,44 @@
-const calcBody = document.getElementById('calculator-body');
-console.log(calcBody);
-const buttonGrid = calcBody.querySelector('button-grid');
+//Basic maths functions
 
-const buttons = buttonGrid.querySelectorAll('div');
+function add(a,b){
+    return a + b;
+}
 
-buttons.forEach(button => {
-    button.classList.add('button');
-});
+function subtract(a,b){
+    return a - b;
+}
+
+function multiply(a,b){
+    return a * b;
+}
+
+function divide(a,b){
+    return a / b;
+}
+
+//Operate function to perform a given action on two numbers
+function operate(a,b,operator){
+    if (operator == "x"){
+        return multiply(a,b);
+    }
+}
+
+//We need event listeners on the number buttons that display the just
+//pressed key in the display. We need to treat '.' differently though as
+//we don't want users to be able to add more than one decimal point!
+
+let numberButtons = document.querySelectorAll('.number');
+let screen = document.querySelector('#screen-text')
+screen.innerHTML = '0';
+
+numberButtons.forEach((item, index) => numberButtons[index].addEventListener('click', numButtonPress));
+
+function numButtonPress(e){
+    let input = this.innerHTML;
+    if(screen.innerHTML == '0'){
+        screen.innerHTML = input;
+    }else{
+    screen.innerHTML += input;
+    }
+}
+
