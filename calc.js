cf68e99cd040b774d0017091ad6136f6205ca965
Chris Johnson <chrisjohnson1138@gmail.com>
2021-05-04 21:00:28 +0100

    Got all basic maths functions working and organised code.

--- calc.js
+++ calc.js
@@ -1,5 +1,29 @@
-//Basic maths functions
+//Get page elements
+let numberButtons = document.querySelectorAll('.number');
+let screen = document.querySelector('#screen-text')
+let clearButton = document.getElementById('clr');
+let deleteButton = document.getElementById('del');
+let operatorButtons = document.querySelectorAll('.operator');
+let equalsButton = document.getElementById('equals');
 
+//Initial variables and states
+let currentNumber = '0';
+let prevNumber = '0';
+let lastAnswer = 0;
+let lastOperator = '';
+let operatorToggle = 'off';
+let equalsToggle = 'off';
+screen.innerHTML = '0';
+
+//Add listeners
+numberButtons.forEach((item, index) => numberButtons[index].addEventListener('click', numButtonPress));
+operatorButtons.forEach((item, index) => operatorButtons[index].addEventListener('click', operatorPressed));
+clearButton.addEventListener('click', clear);
+deleteButton.addEventListener('click', del);
+equalsButton.addEventListener('click', equalsPressed);
+
+
+//Basic maths functions
 function add(a,b){
     return a + b;
 }
@@ -16,46 +40,45 @@ function divide(a,b){
     return a / b;
 }
 
+//Main functions
+function clear(){
+    currentNumber = '0';
+    lastNumber = '0';
+    operatorToggle = 'off';
+    equalsToggle = 'off';
+    lastAnswer = 0;
+    screen.innerHTML = '0';
+}
 
-//We need event listeners on the number buttons that display the just
-//pressed key in the display. We need to treat '.' differently though as
-//we don't want users to be able to add more than one decimal point!
-
-let numberButtons = document.querySelectorAll('.number');
-let screen = document.querySelector('#screen-text')
-screen.innerHTML = '0';
-
-numberButtons.forEach((item, index) => numberButtons[index].addEventListener('click', numButtonPress));
-
-let currentNumber = '0';
-let lastAnswer = 0;
-let lastOperator = '';
+function operate(){
+    x = parseInt(prevNumber);
+    y = parseInt(currentNumber);
+    if(lastOperator == 'add'){
+        lastAnswer = add(x, y);
+    }else if(lastOperator == 'subtract'){
+        lastAnswer = subtract(x,y);
+    }else if(lastOperator == 'divide'){
+        lastAnswer = divide(x,y);
+    }else if(lastOperator = 'multiply'){
+        lastAnswer = multiply(x,y);
+    }
+}
 
 function numButtonPress(e){
     let input = this.innerHTML;
     if (currentNumber.length == 15){
         return;
     }
-    if(screen.innerHTML == '0' || lastOperator == ''){
+    if(screen.innerHTML == '0' || operatorToggle == 'on' || equalsToggle == 'on'){
         currentNumber = input;
         screen.innerHTML = currentNumber;
     }else{
         currentNumber += input;
         screen.innerHTML = currentNumber;
     }
+    operatorToggle = 'off';
+    equalsToggle = 'off';
 }
-
-let clearButton = document.getElementById('clr');
-let deleteButton = document.getElementById('del');
-
-clearButton.addEventListener('click', clear);
-
-function clear(){
-    currentNumber = '0';
-    screen.innerHTML = currentNumber;
-}
-
-deleteButton.addEventListener('click', del);
 
 function del() {
     if (currentNumber.length == 1){
@@ -66,26 +89,22 @@ function del() {
     screen.innerHTML = currentNumber;
 }
 
-//When someone presses an operator button (plus, minus etc) we need to keep the first number on the screen,
-//then, when they start typing more, store the number on the screen somewhere, record the required operator
-//and display the new number. Pressing = at this point should execute the required sum, howevever we also 
-//need to be able to chain sums. We don't need to be able to undo, so all we really need is to keep the result
-//of the last equation to use on the next. Pressing an operator key should show the answer to the previous
-//equation on the screen
+function operatorPressed(e){
+    if(operatorToggle == 'on'){
+        operate();
+        screen.innerHTML = lastAnswer;
+    }
+    prevNumber = currentNumber;
+    currentNumber = screen.innerHTML;
+    lastOperator = this.id;
+    operatorToggle = 'on';
+}
 
-//Define a function to add the current number on the screen to the next number entered
-//First add an event listener to all the operator buttons
-let operatorButtons = document.querySelectorAll('.operator');
+function equalsPressed(){
+    operate();
+    screen.innerHTML = lastAnswer;
+    currentNumber = lastAnswer;
+    equalsToggle = 'on';
 
-console.log(operatorButtons);
-
-operatorButtons.forEach((item, index) => operatorButtons[index].addEventListener('click', operate));
-
-//Operate function to perform a given action on two numbers
-function operate(e){
-        currentNumber = screen.innerHTML;
-        lastOperator = this.id;
-        console.log(currentNumber);
-        console.log(lastOperator);
 }
 
