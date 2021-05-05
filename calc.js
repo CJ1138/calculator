13d567bed270e2d4013871b7c24ceeb3238a9b58
CJ1138 <chrisjohnson1138@gmail.com>
2021-05-05 15:08:41 +0100

    Fixed the code to allow for chaining equations together.

--- calc.js
+++ calc.js
@@ -13,14 +13,17 @@ let lastAnswer = 0;
 let lastOperator = '';
 let operatorToggle = 'off';
 let equalsToggle = 'off';
-screen.innerHTML = '0';
+resetScreen();
+unsetOperator()
+let operatorSet = 'no'
+
 
 //Add listeners
 numberButtons.forEach((item, index) => numberButtons[index].addEventListener('click', numButtonPress));
 operatorButtons.forEach((item, index) => operatorButtons[index].addEventListener('click', operatorPressed));
 clearButton.addEventListener('click', clear);
 deleteButton.addEventListener('click', del);
-equalsButton.addEventListener('click', equals);
+equalsButton.addEventListener('click', equalsPressed);
 
 
 //Basic maths functions
@@ -42,16 +45,28 @@ function divide(a,b){
 
 //Main functions
 
-//
-function clear(){
-    currentNumber = '0';
-    lastNumber = '0';
-    operatorToggle = 'off';
-    equalsToggle = 'off';
-    lastAnswer = 0;
+function resetScreen(){
     screen.innerHTML = '0';
 }
 
+function unsetOpHighlight(){
+    operatorButtons.forEach((item, index) => operatorButtons[index].classList.remove('op-clicked'));
+}
+
+//Returns calculator to intitial state
+function clear(){
+    currentNumber = '0';
+    prevNumber = '0';
+    operatorToggle = 'off';
+    equalsToggle = 'off';
+    lastAnswer = 0;
+    unsetOpHighlight()
+    resetScreen();
+    let operatorSet = 'no'
+}
+
+//Based on current value of lastOperator calls one of the basic maths functions
+//on currentNumber and prevNumber
 function operate(){
     x = parseInt(prevNumber);
     y = parseInt(currentNumber);
@@ -66,43 +81,68 @@ function operate(){
     }
 }
 
+//Runs when a number button is pressed
 function numButtonPress(e){
+    if(operatorSet == 'yes'){
+        unsetOpHighlight();
+    }
+    //Declares a var called input and sets it to the current value on the screen
     let input = this.innerHTML;
+    //Exits out of this function if the screen is already full (MAYBE ADD A VISUAL CHANGE)
     if (currentNumber.length == 15){
         return;
     }
+    //Overwrites currentNumber with the last entered value
+    //Replaces screen text with the last entered value
+    //Does this only if screen displays '0' or if an operator or equals was just pressed
     if(screen.innerHTML == '0' || operatorToggle == 'on' || equalsToggle == 'on'){
         currentNumber = input;
-        screen.innerHTML = currentNumber;
+        screen.innerHTML = input;
     }else{
+    //If none of the above conditions is met, concatente the last entered number on the screen
+    //Also upate the value of currentNumber (a string) by concating the last entered number on the end
         currentNumber += input;
-        screen.innerHTML = currentNumber;
+        screen.innerHTML += input;
     }
+    //Set that the last entered key was not equals or an operator
     operatorToggle = 'off';
     equalsToggle = 'off';
 }
 
+//Runs when the delete button is pressed
 function del() {
-    if (currentNumber.length == 1){
+    //If only one number is on the screen, set the current number to 0 
+    if (screen.innerHTML.length == 1){
         currentNumber = '0';
+    //If more than one number is on the screen, just deleted the last char of current number
     } else {
         currentNumber = currentNumber.slice(0, -1);
     }
+    //Update the screen to the present value of current number
     screen.innerHTML = currentNumber;
 }
 
 function operatorPressed(e){
-    if(operatorToggle == 'on'){
+    unsetOpHighlight()
+    this.classList.add('op-clicked');
+    
+    if(operatorSet == 'yes'){
         operate();
+        currentNumber = lastAnswer;
         screen.innerHTML = lastAnswer;
+        unsetOpHighlight()
     }
+
     prevNumber = currentNumber;
     currentNumber = screen.innerHTML;
     lastOperator = this.id;
     operatorToggle = 'on';
+    operatorSet = 'yes';
+    this.classList.add('op-clicked');
 }
 
 function equalsPressed(){
+    unsetOpHighlight()
     operate();
     screen.innerHTML = lastAnswer;
     currentNumber = lastAnswer;
@@ -110,3 +150,4 @@ function equalsPressed(){
 
 }
 
+//When someone enters a number and presses + then enters another 
