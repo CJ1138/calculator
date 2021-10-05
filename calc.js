0a8184812329d9f1f8fd47baa7e90cabca568e96
Chris Johnson <chrisjohnson1138@gmail.com>
2021-10-05 21:42:12 +0100

    Added variables to track current program state

--- calc.js
+++ calc.js
@@ -1,8 +1,32 @@
 //Get page elements
-let numberButtons = document.querySelectorAll('.number');
-let screen = document.querySelector('#screen-text')
-let clearButton = document.getElementById('clr');
-let deleteButton = document.getElementById('del');
-let operatorButtons = document.querySelectorAll('.operator');
-let equalsButton = document.getElementById('equals');
+const numberButtons = document.querySelectorAll('.number');
+const screen = document.querySelector('#screen-text')
+const clearButton = document.getElementById('clr');
+const deleteButton = document.getElementById('del');
+const operatorButtons = document.querySelectorAll('.operator');
+const equalsButton = document.getElementById('equals');
+const decimalButtin = document.getElementById('decimal');
+
+/*
+
+Things we need:
+
+- Something to check if the program is in its initial state
+- Something to say what happens when the program is in its initial state (i.e
+    to control how numbers should be initially displayed, and to do nothing except in the case of a number or a decimal)
+- Something to check if an answer is onscreen
+- A variable to hold the current operator (to be set to null when the memory is cleared)
+- A variable to track the number of characters onscreen and logic to say when its full
+- A variable to say when there's a decimal onscreen
+- A variable to store the current number (i.e. the number stored when an operator is pressed)
+- Fucntions to do the maths
+
+*/
+
+// Variables to track current program state:
+var initialState = true;
+var numEntered = null;
+var currentOperator = null;
+var numDisplayed = null;
+var answerDisplayed = false;
 
